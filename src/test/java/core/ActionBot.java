d078033bb9b1ee00d9dc17c91deb469f93c788b7
Łukasz Bartulski <l.bartulski@gmail.com>
2026-03-21 00:19:18 +0100

    initial project structure with bot pattern

---
+++ src/test/java/core/ActionBot.java
@@ -0,0 +1,87 @@
+package core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ActionBot {
+    private final WebDriver driver;
+    private final String baseURL;
+    private WebDriverWait wait;
+    protected String firstName = "Antoni";
+    protected String lastName = "Kowalski";
+    protected String postalCode = "123456";
+
+    public ActionBot(WebDriver driver, String baseURL, WebDriverWait wait) {
+        this.driver = driver;
+        this.baseURL = baseURL;
+        this.wait = wait;
+
+    }
+
+
+    //Methods
+
+    public void click(String cssSelector) {
+        driver.findElement(By.cssSelector(cssSelector)).click();
+
+    }
+
+    public void waitForPresenceOfElementLocated(String cssSelector) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(cssSelector)));
+    }
+
+    public List<WebElement> getElements(String cssSelector) {
+        return driver.findElements(By.cssSelector(cssSelector));
+    }
+
+    public String getTextString(String cssSelector) {
+        return driver.findElement(By.cssSelector(cssSelector)).getText();
+    }
+
+
+    public int loopClicker(List<WebElement> cssSelector) {
+        int i = 0;
+        for (WebElement element : cssSelector) {
+            i++;
+            element.click();
+            System.out.println(i + ". : " + "click!");
+
+        }
+        return i;
+
+    }
+
+    //two arguments - cssSelector, string
+    public void sendKeys(String cssSelector, String string) {
+        driver.findElement(By.cssSelector(cssSelector)).sendKeys(string);
+
+    }
+
+    public double parseStringToDouble(String cssSelector) {
+        String stringValue = driver.findElement(By.cssSelector(cssSelector)).getText().replaceAll("[^0-9.]", "");
+        double doubleValue = Double.parseDouble(stringValue);
+
+        return doubleValue;
+    }
+
+    public String getURL() {
+        return driver.getCurrentUrl();
+    }
+
+    public void login(String userName, String password) {
+        driver.get(baseURL);
+        driver.findElement(By.cssSelector("#user-name"))
+                .sendKeys(userName);
+        driver.findElement(By.cssSelector("#password"))
+                .sendKeys(password);
+        driver.findElement(By.cssSelector("#login-button"))
+                .click();
+    }
+}
---
+++ src/test/java/core/TestBase.java
@@ -0,0 +1,51 @@
+package core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestBase {
+    protected WebDriver driver;
+    private WebDriverWait wait;
+    protected ActionBot bot;
+
+    protected final String baseURL = "https://www.saucedemo.com";
+    protected final String username = "standard_user";
+    protected final String password = "secret_sauce";
+
+
+    @BeforeEach
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("profile.password_manager_enabled", false);
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_leak_detection", false);
+
+        options.setExperimentalOption("prefs", prefs);
+        driver = new ChromeDriver(options);
+        bot = new ActionBot(driver, baseURL, wait);
+        driver.get(baseURL);
+
+        driver.findElement(By.cssSelector("#user-name"))
+                .sendKeys(username);
+        driver.findElement(By.cssSelector("#password"))
+                .sendKeys(password);
+        driver.findElement(By.cssSelector("#login-button"))
+                .click();
+    }
+
+    @AfterEach
+    public void quit() {
+        driver.quit();
+    }
+
+
+}
