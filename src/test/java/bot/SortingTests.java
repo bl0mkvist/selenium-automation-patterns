223bd673e6d4f23c11cd516c8fa55b02b3b93905
Łukasz Bartulski <l.bartulski@gmail.com>
2026-03-22 21:46:50 +0100

    update sorting tests

--- src/test/java/bot/SortingTests.java
+++ src/test/java/bot/SortingTests.java
@@ -1,16 +1,75 @@
 package bot;
 
 import core.TestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SortingTests extends TestBase {
+
+    private final String sortingContainer = ".product_sort_container";
+    private final String sortAtoZButton = "option[value='az']";
+    private final String sortZtoAButton = "option[value='za']";
+
+    private final String listProductsHeaders = ".inventory_item_name ";
+
+    @Test
+    @DisplayName("Sorts products by name A to Z")
+    void shouldSortProductsAlphabetically() {
+
+        bot.validLogin();
+        bot.click(sortingContainer);
+        bot.click(sortAtoZButton);
+
+        List<WebElement> headersList = bot.getElements(listProductsHeaders);
+        List<String> actualHeadersList = new ArrayList<>();
+
+        for (WebElement header : headersList){
+            actualHeadersList.add(header.getText().trim());
+        }
+
+        List<String> expectedSortedHeadersList = new ArrayList<>(actualHeadersList);
+        Collections.sort(expectedSortedHeadersList, String.CASE_INSENSITIVE_ORDER);
+
+        Assertions.assertEquals(expectedSortedHeadersList, actualHeadersList, "Products are not sorted by name A to Z");
+
+    }
+
+    @Test
+    @DisplayName("Sorts products by name Z to A")
+    void shouldSortProductsAlphabeticallyDescending(){
+        bot.validLogin();
+        bot.click(sortingContainer);
+        bot.click(sortZtoAButton);
+
+        List<WebElement> headersList = bot.getElements(listProductsHeaders);
+        List<String> actualHeadersList = new ArrayList<>();
+
+        for (WebElement header : headersList) {
+            actualHeadersList.add(header.getText().trim());
+        }
+
+        List<String> expectedSortedHeaderList = new ArrayList<>(actualHeadersList);
+        Collections.sort(expectedSortedHeaderList, String.CASE_INSENSITIVE_ORDER.reversed());
+
+        Assertions.assertEquals(expectedSortedHeaderList, actualHeadersList, "Products are not sorted by name Z to A");
+    }
+
+
 }
 
 /*
 Sortowanie
 1.
-Sortowanie A→Z → sprawdź, że nazwy rosną alfabetycznie. (id.scribd.com)
+Sortowanie A→Z → sprawdź, że nazwy rosną alfabetycznie. (id.scribd.com) - done
 2.
-Sortowanie Z→A → sprawdź, że nazwy maleją alfabetycznie. (id.scribd.com)
+Sortowanie Z→A → sprawdź, że nazwy maleją alfabetycznie. (id.scribd.com) - done
 3.
 Sortowanie cena Low→High → ceny rosną. (id.scribd.com)
 4.
