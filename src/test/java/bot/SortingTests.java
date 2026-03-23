ccc0e310235ca3cc696d175821e2906bc573d983
Łukasz Bartulski <l.bartulski@gmail.com>
2026-03-23 14:31:11 +0100

    add sorting tests

--- src/test/java/bot/SortingTests.java
+++ src/test/java/bot/SortingTests.java
@@ -6,18 +6,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.math.BigDecimal;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SortingTests extends TestBase {
 
     private final String sortingContainer = ".product_sort_container";
-    private final String sortAtoZButton = "option[value='az']";
-    private final String sortZtoAButton = "option[value='za']";
-
+    private final String sortingContainerSelectedValue = ".product_sort_container option:checked";
+    private final String sortAtoZButton = "select>option[value='az']";
+    private final String sortZtoAButton = "select>option[value='za']";
+    private final String sortPriceLtoHButton = "select>option[value='lohi']";
+    private final String sortPriceHtoLButton = "select>option[value='hilo']";
     private final String listProductsHeaders = ".inventory_item_name ";
+    private final String listProductPrices = ".inventory_item_price";
 
     @Test
     @DisplayName("Sorts products by name A to Z")
@@ -62,6 +70,35 @@ public class SortingTests extends TestBase {
     }
 
 
+    @Test
+    @DisplayName("Sorts products by price Lowest to Highest")
+    void shouldSortProductsByPriceLowToHigh(){
+        bot.validLogin();
+        bot.click(sortingContainer);
+        bot.click(sortPriceLtoHButton);
+        bot.waitTextToBePresentInElementLocated(sortingContainerSelectedValue, "Price (low to high)");
+
+        String selectedOption = bot.getTextString(sortingContainerSelectedValue).trim();
+        Assertions.assertEquals("Price (low to high)", selectedOption
+                , "Sorting method does not change");
+
+        List<WebElement> productPrices = bot.getElements(listProductPrices);
+        Assertions.assertFalse(productPrices.isEmpty()
+                , "List is empty, possible issue with loading elements on Products Page");
+
+        List<BigDecimal> actualBigDecimalProductPrices = new ArrayList<>();
+
+        for (WebElement price : productPrices) {
+            String stringPrice = price.getText().replace("$", "").trim();
+            actualBigDecimalProductPrices.add(new BigDecimal((stringPrice)));
+        }
+
+        List<BigDecimal> expectedProductPrices = new ArrayList<>(actualBigDecimalProductPrices);
+        expectedProductPrices.sort(Comparator.naturalOrder());
+
+        Assertions.assertEquals(expectedProductPrices, actualBigDecimalProductPrices,
+                "Prices are not sorted by price ascending");
+    }
 }
 
 /*
